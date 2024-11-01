d508e60e95da3414b930c3735d89e63c081ef098
tt50 <156944954+tt50@users.noreply.github.com>
2024-11-01 18:27:21 -0400

    Added javaFx project structure and intellij configuration.
    Added javaFx project structure and intellij configuration.

---
+++ JavaFxModule_PasswordManager/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.javafxmodule_passwordmanager {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.javafxmodule_passwordmanager to javafx.fxml;
+    exports com.example.javafxmodule_passwordmanager;
+}

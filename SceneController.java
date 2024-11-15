c4533d829d2ab9457806a3faae76addc0c122120
tun67213 <tun67213@temple.edu>
2024-11-14 19:16:17 -0500

    Imported moduel to use FXML

--- SceneController.java
+++ SceneController.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.event.ActionEvent;
 import java.io.IOException;
+import javafx.fxml.FXML;
 
 public class SceneController extends Application {
     private Stage stage;

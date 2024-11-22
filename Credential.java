bd2ada76e676695630a482de4b9b9c7432d03b01
tun67213 <tun67213@temple.edu>
2024-11-21 21:04:43 -0500

    Added comments to class for things to be implemented

--- Credential.java
+++ Credential.java
@@ -1,5 +1,39 @@
 
 public class Credential
 {
-	
+	// Variables
+
+	// 	1) For saving nickname
+
+	// 	2) For saving username
+
+	// 	3) For saving encrypted password
+
+	// 	4) For saving encryption key
+
+	// 	5) For saving note
+
+	// Constructors
+
+
+	// Getter methods
+	// 	Function to retrieve nickname
+
+	// 	Function to retrieve username
+
+	// 	Function to retrieve hashed password
+
+	// 	Function to retrieve the encryption key
+
+	// 	Function to get the note
+
+	// 	Local method to retrieve decrypted password
+
+
+	// Other Functions
+	// 	Function to save to file
+	public void saveToFile()
+	{
+		
+	}
 }
